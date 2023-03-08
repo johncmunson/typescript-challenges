28075dc711fa50b6597c0cafd622797a4e3842db
John Munson <johncmunson91@gmail.com>
2023-03-07 20:25:54 -0600

    init

---
+++ index.ts
@@ -0,0 +1,30 @@
+// 1. Write a function called `compact` that removes all falsy values from an array
+export const compact = () => {}
+
+// 2. Write function called `sumValues` to calculate the sum of all the number in an array
+
+// 3. Write a function called `addOrRemove` that adds an item to an array if it doesn't already exist, and removes it if not.
+//    The array can be heterogeneous, but only contain primitive values.
+
+// 4. Write a function called `chunk` that accepts an array and splits it into groups of length `size`. If the array can't be split evenly, the final chunk will be the remaining elements.
+
+// 5. Write a function called `arrayDifference` that returns all the values from the first array that are not present in any of the other provided arrays
+
+// 6. Write a function called `isPalindrome` that returns true if a string is a palindrome, and false otherwise
+
+// 7. Write a function called `getAllKeys` that returns all the keys of a nested object
+
+// 8. Write a class called `Calculator` with the following requirements...
+//      - may optionally be initialized with a starting value, otherwise it starts with zero in the register
+//      - has the ability to add, subtract, multiply, and divide
+//      - calculations may be chained together (fluent API)
+//      - the register can be reset to zero using a `clear` method
+//      - returns the calculated result when `calculate` is called
+//    Example:
+//      const myCalc = new Calculator(3)
+//      const result = myCalc.add(2).multiply(10).calculate()
+//      console.log(result) //=> 50
+
+// 9. Write a function called `sleep` that awaits a given number of milliseconds before resolving
+
+// 10. Write a function called `memoize` that memoizes a function that accepts a variable number of arguments and returns it
